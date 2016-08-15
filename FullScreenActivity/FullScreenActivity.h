39d10df72661a9d3a0f36acb6f8c2667560ffa3a
Damon <damon@damondembp.lan>
2016-08-15 14:22:47 +0800

    Initial Commit

---
+++ FullScreenActivity/FullScreenActivity.h
@@ -0,0 +1,19 @@
+//
+//  FullScreenActivity.h
+//  FullScreenActivity
+//
+//  Created by Damon on 16/8/15.
+//  Copyright © 2016年 Damon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FullScreenActivity.
+FOUNDATION_EXPORT double FullScreenActivityVersionNumber;
+
+//! Project version string for FullScreenActivity.
+FOUNDATION_EXPORT const unsigned char FullScreenActivityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FullScreenActivity/PublicHeader.h>
+
+
